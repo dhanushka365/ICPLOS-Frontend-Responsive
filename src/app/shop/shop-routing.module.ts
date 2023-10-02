2c7b0e9fb6e0c94e00fd655b6d43ccc2be53e20c
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-10-02 12:22:31 +0530

    works commited

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop.component';
 
-
+const routes: Routes = [
+  {path:'shop', component:ShopComponent},
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forChild(routes)
+  ],
+  exports:[RouterModule]
 })
 export class ShopRoutingModule { }
