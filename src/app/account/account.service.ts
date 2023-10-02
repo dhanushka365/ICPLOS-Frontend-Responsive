edf4fb0409499a5280045f8e207cbd7b538af17a
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-10-02 12:02:31 +0530

    folder struture corrected

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../shared/models/user';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  baseUrl = 'https://localhost:7184/api/';
+  constructor( private http:HttpClient) { }
+
+  getProducts(): Observable<User[]>{
+    return this.http.get<User[]>(this.baseUrl + 'Products');
+  }
 }
