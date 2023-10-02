d07c9babce8d0acfaa45b142e5112126eafe7d01
dhnaushka365 <pasindudhanushka365@gmail.com>
2023-10-02 14:07:50 +0530

    works commited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { ShopComponent } from './shop/shop.component';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
 
 
 
 const routes: Routes = [
  // {path:'',redirectTo: '/main' , pathMatch:'full'},
-  { path: 'login', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
-  //{path:'main', component:MainComponent}
+  {path:'main', component:MainComponent},
+  {path:'shop', loadChildren:()=>import('./shop/shop.module').then(mod=>mod.ShopModule)},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'**',redirectTo: '/main' , pathMatch:'full'}
   
 ];
 
